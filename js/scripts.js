a0deb693d8fde7defc0f38768e5203cdd238c23d
aploch2 <aploch2@my.wctc.edu>
2021-05-20 15:11:09 -0500

    added javascript for menu icons

--- js/scripts.js
+++ js/scripts.js
@@ -1,12 +1,34 @@
-function MobileNavToggle() {
+function MobileNavShow() {
     let w = document.documentElement.clientWidth || window.innerWidth;
     if(w <= 950) {
-        var x = document.getElementById("primary-nav");
+        let x = document.getElementById("primary-nav");
+        let menuIcon = document.getElementById("menu-icon");
+        let closeIcon = document.getElementById("close-icon");
+
         if (x.className === "primary-nav") {
-          x.className += " responsive-nav";
+          x.classList.add ("responsive-nav");
+          menuIcon.style.display = "none";
+          closeIcon.style.display = "block";
         } else {
           x.className = "primary-nav";
         }
-        console.log('hello');
+        console.log('Show');
     }
-  }
+}
+function MobileNavHide() {
+    let w = document.documentElement.clientWidth || window.innerWidth;
+    if(w <= 950) {
+        let x = document.getElementById("primary-nav");
+        let menuIcon = document.getElementById("menu-icon");
+        let closeIcon = document.getElementById("close-icon");
+
+        if (x.className === "primary-nav responsive-nav") {
+          x.classList.remove("responsive-nav");
+          menuIcon.style.display = "block";
+          closeIcon.style.display = "none";
+        } else {
+          x.className = "primary-nav";
+        }
+        console.log('Hide');
+    }
+}
